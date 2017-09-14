85c3f4905b1ff4fd862258f40e4a878deafd3fba
Jonathan Marks <jmarks1992@users.noreply.github.com>
2017-09-14 14:54:25 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+   
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().split("\n")
+
+setup(
+    name='congress',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1',
+
+    description='A sample Python project',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/QuorumUS/congress',
+
+    # Author details
+    author='The United States IO',
+    author_email='unknown@theunitedstates.io',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=requirements,
+
+)
